e8e416ffcecc233391f1f2bd4924f7677e34f4bb
Anatoly <a.k.soldatov@gmail.com>
2020-07-24 15:33:49 +0300

    went back to the main version with only one tweak for different frame roll behaviour on Add

--- metric.go
+++ metric.go
@@ -2,6 +2,7 @@ package metric
 
 import (
 	"encoding/json"
+	"fmt"
 	"math"
 	"sync"
 	"sync/atomic"
@@ -20,8 +21,20 @@ type Metric interface {
 
 // NewCounter returns a counter metric that increments the value with each
 // incoming number.
-func NewCounter(frame ...time.Duration) Metric {
-	return newMetric(func() Metric { return &counter{} }, frame...)
+func NewCounter(frames ...string) Metric {
+	return newMetric(func() Metric { return &counter{} }, frames...)
+}
+
+// NewGauge returns a gauge metric that sums up the incoming values and returns
+// mean/min/max of the resulting distribution.
+func NewGauge(frames ...string) Metric {
+	return newMetric(func() Metric { return &gauge{} }, frames...)
+}
+
+// NewHistogram returns a histogram metric that calculates 50%, 90% and 99%
+// percentiles of the incoming numbers.
+func NewHistogram(frames ...string) Metric {
+	return newMetric(func() Metric { return &histogram{} }, frames...)
 }
 
 type timeseries struct {
@@ -70,17 +83,40 @@ func (ts *timeseries) Add(n float64) {
 func (ts *timeseries) MarshalJSON() ([]byte, error) {
 	ts.Lock()
 	defer ts.Unlock()
-	data, err := json.Marshal(struct {
+	ts.roll()
+	return json.Marshal(struct {
 		Interval float64  `json:"interval"`
 		Samples  []Metric `json:"samples"`
 	}{float64(ts.interval) / float64(time.Second), ts.samples})
-	ts.roll()
-	return data, err
 }
 
 func (ts *timeseries) String() string {
 	b, _ := ts.MarshalJSON()
 	return string(b)
+}
+
+type multimetric []Metric
+
+func (mm multimetric) Add(n float64) {
+	for _, m := range mm {
+		m.Add(n)
+	}
+}
+func (mm multimetric) Reset() {
+	for _, m := range mm {
+		m.Reset()
+	}
+}
+func (mm multimetric) String() string {
+	s := `{"metrics":[`
+	for i, m := range mm {
+		if i != 0 {
+			s = s + ","
+		}
+		s = s + m.String()
+	}
+	s = s + "]}"
+	return s
 }
 
 func strjson(x interface{}) string {
@@ -104,7 +140,6 @@ func (c *counter) Add(n float64) {
 		}
 	}
 }
-
 func (c *counter) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
 		Type  string  `json:"type"`
@@ -112,35 +147,170 @@ func (c *counter) MarshalJSON() ([]byte, error) {
 	}{"c", c.value()})
 }
 
-func newTimeseries(builder func() Metric, frame ...time.Duration) *timeseries {
-	var interval time.Duration
-	var totalDuration time.Duration
+type gauge struct {
+	sync.Mutex
+	sum   float64
+	min   float64
+	max   float64
+	count int
+}
 
-	if frame[0] == 0 {
+func (g *gauge) String() string { return strjson(g) }
+func (g *gauge) Reset() {
+	g.Lock()
+	defer g.Unlock()
+	g.count, g.sum, g.min, g.max = 0, 0, 0, 0
+}
+func (g *gauge) Add(n float64) {
+	g.Lock()
+	defer g.Unlock()
+	if n < g.min || g.count == 0 {
+		g.min = n
+	}
+	if n > g.max || g.count == 0 {
+		g.max = n
+	}
+	g.sum += n
+	g.count++
+}
+
+func (g *gauge) MarshalJSON() ([]byte, error) {
+	g.Lock()
+	defer g.Unlock()
+	return json.Marshal(struct {
+		Type string  `json:"type"`
+		Mean float64 `json:"mean"`
+		Min  float64 `json:"min"`
+		Max  float64 `json:"max"`
+	}{"g", g.mean(), g.min, g.max})
+}
+func (g *gauge) mean() float64 {
+	if g.count == 0 {
+		return 0
+	}
+	return g.sum / float64(g.count)
+}
+
+const maxBins = 100
+
+type bin struct {
+	value float64
+	count float64
+}
+
+type histogram struct {
+	sync.Mutex
+	bins  []bin
+	total uint64
+}
+
+func (h *histogram) String() string { return strjson(h) }
+func (h *histogram) Reset() {
+	h.Lock()
+	defer h.Unlock()
+	h.bins = nil
+	h.total = 0
+}
+
+func (h *histogram) Add(n float64) {
+	h.Lock()
+	defer h.Unlock()
+	defer h.trim()
+	h.total++
+	newbin := bin{value: n, count: 1}
+	for i := range h.bins {
+		if h.bins[i].value > n {
+			h.bins = append(h.bins[:i], append([]bin{newbin}, h.bins[i:]...)...)
+			return
+		}
+	}
+
+	h.bins = append(h.bins, newbin)
+}
+
+func (h *histogram) MarshalJSON() ([]byte, error) {
+	h.Lock()
+	defer h.Unlock()
+	return json.Marshal(struct {
+		Type string  `json:"type"`
+		P50  float64 `json:"p50"`
+		P90  float64 `json:"p90"`
+		P99  float64 `json:"p99"`
+	}{"h", h.quantile(0.5), h.quantile(0.9), h.quantile(0.99)})
+}
+
+func (h *histogram) trim() {
+	for len(h.bins) > maxBins {
+		d := float64(0)
+		i := 0
+		for j := 1; j < len(h.bins); j++ {
+			if dv := h.bins[j].value - h.bins[j-1].value; dv < d || j == 1 {
+				d = dv
+				i = j
+			}
+		}
+		count := h.bins[i-1].count + h.bins[i].count
+		merged := bin{
+			value: (h.bins[i-1].value*h.bins[i-1].count + h.bins[i].value*h.bins[i].count) / count,
+			count: count,
+		}
+		h.bins = append(h.bins[:i-1], h.bins[i:]...)
+		h.bins[i-1] = merged
+	}
+}
+
+func (h *histogram) quantile(q float64) float64 {
+	count := q * float64(h.total)
+	for i := range h.bins {
+		count -= float64(h.bins[i].count)
+		if count <= 0 {
+			return h.bins[i].value
+		}
+	}
+	return 0
+}
+
+func newTimeseries(builder func() Metric, frame string) *timeseries {
+	var (
+		totalNum, intervalNum   int
+		totalUnit, intervalUnit rune
+	)
+	units := map[rune]time.Duration{
+		's': time.Second,
+		'm': time.Minute,
+		'h': time.Hour,
+		'd': time.Hour * 24,
+		'w': time.Hour * 24 * 7,
+		'M': time.Hour * 24 * 7 * 30,
+		'y': time.Hour * 24 * 7 * 365,
+	}
+	fmt.Sscanf(frame, "%d%c%d%c", &totalNum, &totalUnit, &intervalNum, &intervalUnit)
+	interval := units[intervalUnit] * time.Duration(intervalNum)
+	if interval == 0 {
 		interval = time.Minute
-	} else {
-		interval = frame[0]
 	}
-
-	if frame[1] == 0 {
+	totalDuration := units[totalUnit] * time.Duration(totalNum)
+	if totalDuration == 0 {
 		totalDuration = interval * 15
-	} else {
-		totalDuration = frame[1]
 	}
-
 	n := int(totalDuration / interval)
 	samples := make([]Metric, n, n)
 	for i := 0; i < n; i++ {
 		samples[i] = builder()
 	}
-	return &timeseries{interval: interval, samples: samples}
+	return &timeseries{interval: interval, samples: samples, now: now()}
 }
 
-func newMetric(builder func() Metric, frame ...time.Duration) Metric {
-	// We need to provide both interval and totalDuration to be able to use time series
-	if len(frame) < 2 {
+func newMetric(builder func() Metric, frames ...string) Metric {
+	if len(frames) == 0 {
 		return builder()
 	}
-
-	return newTimeseries(builder, frame...)
+	if len(frames) == 1 {
+		return newTimeseries(builder, frames[0])
+	}
+	mm := multimetric{}
+	for _, frame := range frames {
+		mm = append(mm, newTimeseries(builder, frame))
+	}
+	return mm
 }
--- metric_test.go
+++ metric_test.go
@@ -3,7 +3,7 @@ package metric
 import (
 	"encoding/json"
 	"expvar"
-	"fmt"
+	"math"
 	"math/rand"
 	"reflect"
 	"testing"
@@ -37,60 +37,99 @@ func assertJSON(t *testing.T, o1, o2 interface{}) {
 	} else if err := json.Unmarshal(b2, &expect); err != nil {
 		t.Fatal(err)
 	} else if !reflect.DeepEqual(result, expect) {
-		fmt.Println(result)
-		fmt.Println(expect)
 		t.Fatal(result, expect)
 	}
 }
 
 func TestCounter(t *testing.T) {
-	c := NewCounter()
+	c := &counter{}
 	assertJSON(t, c, h{"type": "c", "count": 0})
 	c.Add(1)
 	assertJSON(t, c, h{"type": "c", "count": 1})
 	c.Add(10)
 	assertJSON(t, c, h{"type": "c", "count": 11})
-}
-
-func TestMetricReset(t *testing.T) {
-	c := &counter{}
-	c.Add(5)
-	assertJSON(t, c, h{"type": "c", "count": 5})
 	c.Reset()
 	assertJSON(t, c, h{"type": "c", "count": 0})
 }
 
-func TestMetricString(t *testing.T) {
-	c := NewCounter()
-	c.Add(1)
-	c.Add(3)
-	if s := c.String(); s != `{"type":"c","count":4}` {
-		t.Fatal(s)
+func TestGauge(t *testing.T) {
+	g := &gauge{}
+	assertJSON(t, g, h{"type": "g", "mean": 0, "min": 0, "max": 0})
+	g.Add(1)
+	assertJSON(t, g, h{"type": "g", "mean": 1, "min": 1, "max": 1})
+	g.Add(5)
+	assertJSON(t, g, h{"type": "g", "mean": 3, "min": 1, "max": 5})
+	g.Add(0)
+	assertJSON(t, g, h{"type": "g", "mean": 2, "min": 0, "max": 5})
+	g.Reset()
+	assertJSON(t, g, h{"type": "g", "mean": 0, "min": 0, "max": 0})
+}
+
+func TestHistogram(t *testing.T) {
+	hist := &histogram{}
+	assertJSON(t, hist, h{"type": "h", "p50": 0, "p90": 0, "p99": 0})
+	hist.Add(1)
+	assertJSON(t, hist, h{"type": "h", "p50": 1, "p90": 1, "p99": 1})
+	hist.Reset()
+	for i := 0; i < 100; i++ {
+		hist.Add(float64(i))
+	}
+	assertJSON(t, hist, h{"type": "h", "p50": 49, "p90": 89, "p99": 98})
+}
+
+func TestHistogramNormalDist(t *testing.T) {
+	hist := &histogram{}
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 10000; i++ {
+		hist.Add(rand.Float64() * 10)
+	}
+	b, _ := hist.MarshalJSON()
+	p := h{}
+	json.Unmarshal(b, &p)
+	if math.Abs(p["p50"].(float64)-5) > 0.5 {
+		t.Fatal(p["p50"])
+	}
+	if math.Abs(p["p90"].(float64)-9) > 0.5 {
+		t.Fatal(p["p90"])
+	}
+	if math.Abs(p["p99"].(float64)-10) > 0.5 {
+		t.Fatal(p["p99"])
 	}
 }
 
-func TestCounterTimeline(t *testing.T) {
+func TestTimeline(t *testing.T) {
 	now = mockTime(0)
-	c := NewCounter(1*time.Second, 3*time.Second)
+	c := NewCounter("3s1s")
 	count := func(x float64) h { return h{"type": "c", "count": x} }
 	assertJSON(t, c, h{"interval": 1, "samples": v{count(0), count(0), count(0)}})
 	c.Add(1)
 	assertJSON(t, c, h{"interval": 1, "samples": v{count(1), count(0), count(0)}})
 	now = mockTime(1)
-	_ = c.String()
 	assertJSON(t, c, h{"interval": 1, "samples": v{count(0), count(1), count(0)}})
 	c.Add(5)
 	assertJSON(t, c, h{"interval": 1, "samples": v{count(5), count(1), count(0)}})
 	now = mockTime(3)
-	_ = c.String()
 	assertJSON(t, c, h{"interval": 1, "samples": v{count(0), count(0), count(5)}})
 }
 
 func TestExpVar(t *testing.T) {
+	now = mockTime(0)
 	expvar.Publish("test:count", NewCounter())
+	expvar.Publish("test:timeline", NewCounter("3s1s"))
 	expvar.Get("test:count").(Metric).Add(1)
+	expvar.Get("test:timeline").(Metric).Add(1)
 	if expvar.Get("test:count").String() != `{"type":"c","count":1}` {
 		t.Fatal(expvar.Get("test:count"))
+	}
+	if expvar.Get("test:timeline").String() != `{"interval":1,"samples":[{"type":"c","count":1},{"type":"c","count":0},{"type":"c","count":0}]}` {
+		t.Fatal(expvar.Get("test:timeline"))
+	}
+	now = mockTime(1)
+	if expvar.Get("test:count").String() != `{"type":"c","count":1}` {
+		t.Fatal(expvar.Get("test:count"))
+	}
+	if expvar.Get("test:timeline").String() != `{"interval":1,"samples":[{"type":"c","count":0},{"type":"c","count":1},{"type":"c","count":0}]}` {
+		t.Fatal(expvar.Get("test:timeline"))
 	}
 }
 
@@ -101,8 +140,32 @@ func BenchmarkMetrics(b *testing.B) {
 			c.Add(rand.Float64())
 		}
 	})
+	b.Run("gauge", func(b *testing.B) {
+		c := &gauge{}
+		for i := 0; i < b.N; i++ {
+			c.Add(rand.Float64())
+		}
+	})
+	b.Run("histogram", func(b *testing.B) {
+		c := &histogram{}
+		for i := 0; i < b.N; i++ {
+			c.Add(rand.Float64())
+		}
+	})
 	b.Run("timeline/counter", func(b *testing.B) {
-		c := NewCounter(1*time.Second, 10*time.Second)
+		c := NewCounter("10s1s")
+		for i := 0; i < b.N; i++ {
+			c.Add(rand.Float64())
+		}
+	})
+	b.Run("timeline/gauge", func(b *testing.B) {
+		c := NewGauge("10s1s")
+		for i := 0; i < b.N; i++ {
+			c.Add(rand.Float64())
+		}
+	})
+	b.Run("timeline/histogram", func(b *testing.B) {
+		c := NewHistogram("10s1s")
 		for i := 0; i < b.N; i++ {
 			c.Add(rand.Float64())
 		}
